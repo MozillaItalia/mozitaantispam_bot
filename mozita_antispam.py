a8395d3907f4897c654b3bc6b47fc8ef0f31ae31
Saverio Morelli <saverio.morelli@outlook.it>
2018-11-16 00:15:40 +0100

    updated

--- mozita_antispam.py
+++ mozita_antispam.py
@@ -3,14 +3,19 @@ import time
 from telepot.loop import MessageLoop
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
 from datetime import datetime
-import calendar
 
 TOKEN="---NASCOSTO---"
 
 #COPIARE E INCOLLARE DA QUI - IL TOKEN E' GIA' INSERITO
 
-versione="0.0.1 preview"
-ultimoAggiornamento="15-11-2018"
+versione="0.0.5 preview"
+ultimoAggiornamento="16-11-2018"
+
+VUtenti=[240188083,75870906]#295348075
+VUtenti_name=["mone27"]
+TUtenti=[]
+SUtenti=[]
+#V = Verified - T = Temporary - S = Spam
 
 def risposte(msg):
     localtime=datetime.now()
@@ -35,33 +40,28 @@ def risposte(msg):
     ##I try-except precedenti servono per assegnare, in qualunque circostanza, chat_id e text corettamente (in base al caso)
     print("Testo: "+str(text)+" - Chat: "+str(chat_id)+" - Utente: "+str(user_name)+" - Id Utente: "+str(user_id)+" - Id Messaggio: "+str(message_id))
 
-    start = InlineKeyboardMarkup(inline_keyboard=[
+    new = InlineKeyboardMarkup(inline_keyboard=[
                     [InlineKeyboardButton(text='Leggi il Regolamento', url='https://github.com/Sav22999/Guide/blob/master/Mozilla%20Italia/Telegram/regolamento.md')],
                 ])
 
     #response = bot.getUpdates()
     #print(response)
 
-    VUtenti=[295348075,75870906]
-    VUtenti_name=["mone27"]
-    TUtenti=[240188083]
-    SUtenti=[]
-    #V = Verified - T = Temporary - S = Spam
-
     if user_id in VUtenti or str(user_name) in VUtenti_name:
         print ("Utente verificato!")
         #bot.sendMessage(chat_id, "@"+str(user_name)+" è un utente verificato !")
     elif user_id in TUtenti:
         print ("Utente non verificato!")
-        #bot.sendMessage(chat_id, "@"+str(user_name)+" non è stato verificato !")
-        bot.deleteMessage(chat_id,message_id)
+        bot.deleteMessage(telepot.message_identifier(msg))
+        bot.sendMessage(chat_id, "@"+str(user_name)+" non è stato verificato: Messaggio eliminato.")
     elif user_id in SUtenti:
         print ("Utente spam")
-        bot.sendMessage(chat_id, "@"+str(user_name)+" è un utente spam !")
+        bot.kickChatMember(chat_id, user_id, until_date=None)
+        bot.sendMessage(chat_id, "@"+str(user_name)+" è stato bannato !")
     else:
-        print ("Error: 03")
-
-    #bot.sendMessage(chat_id, text, reply_markup=start)
+        bot.sendMessage(chat_id, "@"+user_name+", benvenuto nel gruppo ufficiale di Mozilla Italia!", reply_markup=new)
+        TUtenti.append(int(user_id))
+        #utente da inserire nella lista momentanea -> nuovo utente
 
 bot=telepot.Bot(TOKEN)
 MessageLoop(bot, {'chat': risposte, 'callback_query': risposte}).run_as_thread()
